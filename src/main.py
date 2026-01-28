047cb212a531e7eecd78bd91df8a3254c792e2cc
Tun Kedsaro <tun.k@terradigitalventures.com>
2026-01-28 19:38:07 +0700

    chore(project) : initialize dev/prod Docker setup, CI/CD, and project structure (deploy v.1.1.0)

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from time import time
 
 app = FastAPI(
     title="Hyde Feed and Cource recommentdation",
-    version="0.0.1",
+    version="1.1.0",
     description=(
         "Hyde Feed and Cource recommentdation (In progress krub)"
         "<br>"
@@ -42,5 +42,66 @@ def health_fastapi():
     return {
         "status": "ok", 
         "service": "FastAPI",
+        "response_time" : f"{process_time:.5f} s"
+        }
+
+from google import genai
+import re
+import json
+import os
+
+
+### Health & Metadata.API:01 ################################################
+@app.get(
+    "/health/gemini", 
+    tags=["Health & Metadata"],
+    description="API:02 Connectivity health check for Gemini LLM service. Verifies API availability and measures round-trip response latency."
+)
+
+def health_gemini():
+    start_time  = time()
+    client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+    resp = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="ping"
+    )
+    finish_time = time()
+    process_time = finish_time - start_time
+    return {
+        "status": "ok",
+        "reply": resp.text,
+        "latencyresponse_time_sec": f"{process_time:.5f} s"
+    }
+
+
+
+from google.cloud import bigquery
+def get_user_events(user_id: str):
+    client = bigquery.Client()
+    query = """
+        SELECT *
+        FROM `poc-piloturl-nonprod.gold_layer.students`
+        WHERE student_id = @user_id
+    """
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("user_id", "STRING", user_id)
+        ]
+    )
+    rows = client.query(query, job_config=job_config)
+    return [dict(row) for row in rows]
+### Health & Metadata.API:02 ################################################
+@app.get(
+    "/health/bigquery", 
+    tags=["Health & Metadata"],
+    description="API:02 Bq -> project -> FastAPI"
+)
+def health_bq():
+    start_time  = time()
+    finish_time = time()
+    process_time = finish_time - start_time
+    return {
+        "status": "ok", 
+        "body": get_user_events("stu_p001"),
         "response_time" : f"{process_time:.5f} s"
         }
